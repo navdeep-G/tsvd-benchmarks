8b5c9fc6f92731d0713610c664d95e8c28121a53
navdeep-G <mr.navdeepgill@gmail.com>
2018-04-03 18:30:10 -0700

    Update plot script

--- tsvd_plot.R
+++ tsvd_plot.R
@@ -3,14 +3,40 @@ library(reshape2)
 
 #Read in benchmark data
 tsvd <- read.csv("/home/navdeep/tsvd-benchmarks/bench_results.csv")
-tsvd$tsvd_settings <- paste0(tsvd$m, "_", tsvd$n, "_", tsvd$k, "_", tsvd$data_precision)
-tsvd$tsvd_settings <- as.factor(tsvd$tsvd_settings)
 
-#Plot data
-tsvd_sub <- tsvd[,c("tsvd_settings", "h2o4gpu_cusolver", "sklearn_arpack", "h2o4gpu_power", "sklearn_random")]
-tsvd_sub_melt <- melt(tsvd_sub)
+#Collect small, medium, and big matrix cases
+tsvd_small <- subset(tsvd, m < 50000) #For bigger number of rows
+tsvd_small$tsvd_settings <- paste0(tsvd_small$m, "_", tsvd_small$n, "_", tsvd_small$k, "_", tsvd_small$data_precision)
+tsvd_small$tsvd_settings <- as.factor(tsvd_small$tsvd_settings)
+tsvd_sub_small <- tsvd_small[,c("tsvd_settings", "h2o4gpu_cusolver", "sklearn_arpack", "h2o4gpu_power", "sklearn_random")]
+tsvd_sub_small_melt <- melt(tsvd_sub_small)
 
-ggplot(tsvd_sub_melt,aes(x = tsvd_settings,y = value)) +
+tsvd_medium <- subset(tsvd, m == 50000) #For bigger number of rows
+tsvd_medium$tsvd_settings <- paste0(tsvd_medium$m, "_", tsvd_medium$n, "_", tsvd_medium$k, "_", tsvd_medium$data_precision)
+tsvd_medium$tsvd_settings <- as.factor(tsvd_medium$tsvd_settings)
+tsvd_sub_medium <- tsvd_medium[,c("tsvd_settings", "h2o4gpu_cusolver", "sklearn_arpack", "h2o4gpu_power", "sklearn_random")]
+tsvd_sub_medium_melt <- melt(tsvd_sub_medium)
+
+tsvd_large <- subset(tsvd, m > 50000) #For bigger number of rows
+tsvd_large$tsvd_settings <- paste0(tsvd_large$m, "_", tsvd_large$n, "_", tsvd_large$k, "_", tsvd_large$data_precision)
+tsvd_large$tsvd_settings <- as.factor(tsvd_large$tsvd_settings)
+tsvd_sub_large <- tsvd_large[,c("tsvd_settings", "h2o4gpu_cusolver", "sklearn_arpack", "h2o4gpu_power", "sklearn_random")]
+tsvd_sub_large_melt <- melt(tsvd_sub_large)
+
+#Plot benchmarks for small datasets (<50K rows)
+ggplot(tsvd_sub_small_melt,aes(x = tsvd_settings,y = value)) +
   geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
-  labs(title = "Benchmark TSVD", x = "Rows_Columns_k_precision", y = "Time") +
+  labs(title = "Benchmark TSVD (<50K rows)", x = "Rows_Columns_k_precision", y = "Time") +
+  coord_flip()
+
+#Plot benchmarks for medium datasets (=50K rows)
+ggplot(tsvd_sub_medium_melt,aes(x = tsvd_settings,y = value)) +
+  geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
+  labs(title = "Benchmark TSVD (=50K rows)", x = "Rows_Columns_k_precision", y = "Time") +
+  coord_flip()
+
+#Plot benchmarks for large size datasets (>50K rows)
+ggplot(tsvd_sub_large_melt,aes(x = tsvd_settings,y = value)) +
+  geom_bar(aes(fill = variable),stat = "identity",position = "dodge") +
+  labs(title = "Benchmark TSVD on Large Data (>50K rows)", x = "Rows_Columns_k_precision", y = "Time") +
   coord_flip()
